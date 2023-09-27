3e92c893cfdae2fe58003c5c14358df8c31aa290
Agris Jurjans <agris@visikon.com>
2023-09-27 07:55:46 +0200

    api: added cors

--- api/server.ts
+++ api/server.ts
@@ -3,12 +3,16 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 import express from 'express';
 import { getRoutes } from './src/startup/routes';
 import { getAssociations } from './src/models/associations';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 // Importing associations
 getAssociations();
+
+// Middlewares
+app.use(cors());
 
 // Importing routes
 getRoutes(app);
